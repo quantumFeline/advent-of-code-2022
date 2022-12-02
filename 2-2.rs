90d5039a8e1459cf8f584ed1399061d2903ea2e4
Quantum Cat <veronika.netch@gmail.com>
2022-12-02 19:41:01 +0100

    Day 2

---
+++ 2-2.rs
@@ -0,0 +1,25 @@
+fn index(player_move: char) -> usize {
+    let mut ans: usize = 0;
+    if player_move == 'B' || player_move == 'Y' {
+        ans = 1;
+    } else if player_move == 'C' ||player_move == 'Z' {
+        ans = 2;
+    }
+    ans
+}
+
+fn main() {
+
+    let score_table = vec![[3, 4, 8], [1, 5, 9], [2, 6, 7]];
+
+    let input: Vec<&str> = include_str!("./input/2.txt").lines().collect();
+    let mut score: i32 = 0;
+
+    for line in input {
+        let moves: Vec<&str> = line.split(" ").collect();
+        score += score_table[index(moves[0usize].chars().nth(0).unwrap())][index(moves[1usize].chars().nth(0).unwrap())];
+        //println!("{:?} {}",moves, score);
+    }
+
+    println!("{:?}",score);
+}
