7f1adb92fe67dbc9806fa8c8aa818417a68529be
Quantum Cat <veronika.netch@gmail.com>
2022-12-04 11:46:06 +0100

    Day 4

---
+++ 4/4-1.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    let mut sum = 0;
+    for line in include_str!("../input/4.txt").lines() {
+        let assignments: Vec<&str> = line.split(",").collect();
+        let elf1: Vec<i32> = assignments[0].split("-").map(|x| x.parse::<i32>().unwrap()).collect();
+        let elf2: Vec<i32> = assignments[1].split("-").map(|x| x.parse::<i32>().unwrap()).collect();
+        //println!("{:?} {:?}",elf1, elf2);
+        if elf1[0] >= elf2[0] && elf1[1] <= elf2[1] || elf1[0] <= elf2[0] && elf1[1] >= elf2[1] {
+            sum+=1;
+        }
+    }
+
+    println!("{:?}",sum);
+}
