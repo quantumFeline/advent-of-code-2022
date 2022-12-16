25a7e4d1233ef3226a4752b27c69fafd53febb83
Quantum Cat <veronika.netch@gmail.com>
2022-12-16 15:36:21 +0100

    wip

--- 16/16-1.rs
+++ 16/16-1.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 fn main() {
     const SECONDS: usize = 30;
+    const MAX_VALVES_N: usize = 1000;
 
     let s1 = "Valve ";
     let s2 = " has flow rate=";
@@ -9,8 +10,11 @@ fn main() {
     let s3_var = "; tunnel leads to valve ";
     let input = include_str!("../input/16.txt").lines();
 
+    //let mut valve_indices: HashMap<&str, usize> = HashMap::new();
+    //let mut next_valve_i: usize = 0;
+    //let mut valve_flow = vec![0; MAX_VALVES_N];
     let mut valve_flow = HashMap::new();
-    let mut tunnels = HashMap::new();
+    let mut tunnels = HashMap::new(); //: HashMap<usize, Vec<usize>>
 
     for line in input {
         let (valve, valve_info) = [line[s1.len()..].split(s2)].map(|mut x| (x.next().unwrap(), x.next().unwrap()))[0];
@@ -20,10 +24,15 @@ fn main() {
         } else {
             [valve_info.split(s3_var)].map(|mut x| (x.next().unwrap(), x.next().unwrap()))[0]
         };
-        let flow_rate = info1.parse::<u32>().unwrap();
+        let flow_rate = info1.parse::<i32>().unwrap();
         let valve_tunnels = info2.split(", ").collect::<Vec<_>>();
         println!("{:?} {:?} {:?}", valve, flow_rate, valve_tunnels);
 
+//        if !valve_indices.contains(valve) {
+//            valve_indices.insert(valve, next_valve_i);
+//            next_valve_i += 1;
+//        }
+//        let valve_i = valve_indices.get(valve).unwrap();
         valve_flow.insert(valve, flow_rate);
         tunnels.insert(valve, valve_tunnels);
     }
@@ -34,7 +43,6 @@ fn main() {
     let mut dp: Vec<HashMap<&str, i32>> = vec![HashMap::new(); SECONDS];
     let mut dp_open_valves: Vec<HashMap<&str, Vec<&str>>> = vec![HashMap::new(); SECONDS];
     // value = max active flow rate at i-th second, if we're standing in front of j-th valve
-
     for valve in valve_flow.keys() {
         if *valve == "AA" {
             dp[0].insert(valve, 0);
@@ -47,13 +55,13 @@ fn main() {
         for valve in valve_flow.keys() {
             let mut best_flow = VALVE_UNREACHABLE;
             for prev_valve in tunnels.get(valve).unwrap() {
-                let get_there_flow = dp[i-1].get(prev_valve).unwrap();
-                let get_there_and_open_valve_flow = if i != 1 {dp[i-2].get(prev_valve).unwrap() + valve_flow.get(valve).unwrap()} else {VALVE_UNREACHABLE};
+                let get_there_flow = *dp[i-1].get(prev_valve).unwrap();
+                let get_there_and_open_valve_flow = if i != 1 {*dp[i-2].get(prev_valve).unwrap() + valve_flow.get(valve).unwrap()} else {VALVE_UNREACHABLE};
                 if best_flow < get_there_flow { // just get there from neighbouring point by i-th second
                     best_flow = get_there_flow;
                 } if best_flow < get_there_and_open_valve_flow { // get there AND open the valve by that time
                     best_flow = get_there_and_open_valve_flow;
-                    
+                    dp_open_valves[i].get_mut(valve).map(|val| val.push(valve));
                 }
             }
         }
